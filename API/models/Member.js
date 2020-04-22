13939696e326d70acd5bb97412796711c5d13893
youssefwork <youssef.samih97@gmail.com>
2020-04-22 09:08:06 +0000

    first commit

---
+++ API/models/Member.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Member = new Schema({
+  MemberName: {
+    type: String,
+  },
+  MemberBio: {
+    type: String
+  },
+  MemberAge: {
+    type: Number
+  }
+}, {
+  collection: 'Member'
+});
+
+module.exports = mongoose.model('Member', Member);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SlimLoadingBarModule } from 'ng2-slim-loading-bar';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MemberAddComponent } from './member-add/member-add.component';
+import { MemberGetComponent } from './member-get/member-get.component';
+import { MemberEditComponent } from './member-edit/member-edit.component';
+import { MemberService } from './member.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MemberAddComponent,
+    MemberGetComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SlimLoadingBarModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MemberService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
